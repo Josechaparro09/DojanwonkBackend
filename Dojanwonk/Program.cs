420a800c6b6f0a1603c18091e674fe88fa187dc8
Damianq22 <dcamiloquintero@unicesar.edu.co>
2025-05-24 10:03:46 -0500

    Añadir configuración de CORS y nuevos archivos binarios
    Se han añadido varios archivos binarios, incluyendo `CodeChunks.db`, `CodeChunks.db-shm`, `CodeChunks.db-wal`, `SemanticSymbols.db`, `SemanticSymbols.db-shm` y `SemanticSymbols.db-wal`.
    
    En `Program.cs`, se han incluido las directivas `using DAL;` y `using DAL.Modelos;`, y se ha mejorado la legibilidad de la configuración del contexto de la base de datos.
    
    Se ha implementado la configuración de CORS para permitir cualquier origen, encabezado y método, creando una política llamada "AllowAll". Además, se han añadido servicios a la inyección de dependencias, como `DBRango`, `DBGrupo`, `DBEstudiante` y `ServiceEstudiante`.
    
    Finalmente, se ha integrado el middleware de CORS en la configuración de la aplicación, manteniendo las configuraciones existentes para Swagger y el pipeline de solicitudes HTTP.

--- Dojanwonk/Program.cs
+++ Dojanwonk/Program.cs
@@ -1,18 +1,35 @@
 using BLL;
+using DAL;
 using DAL.Modelos;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<DbDojankwonContext>(optionts=>
-optionts.UseSqlServer(builder.Configuration.GetConnectionString("connectionDB")));
-// Add services to the container.
 
+builder.Services.AddDbContext<DbDojankwonContext>(optionts =>
+    optionts.UseSqlServer(builder.Configuration.GetConnectionString("connectionDB")));
+
+// Configurar CORS para permitir cualquier origen, header y método
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// Add services to the container.
 builder.Services.AddScoped<DBUsuario>();
 builder.Services.AddScoped<ServiceUsuario>();
 builder.Services.AddScoped<DBArticulo>();
 builder.Services.AddScoped<ServiceArticulo>();
 builder.Services.AddScoped<DBPrestamo>();
 builder.Services.AddScoped<ServicePrestamo>();
+builder.Services.AddScoped<DBRango>();
+builder.Services.AddScoped<DBGrupo>();
+builder.Services.AddScoped<DBEstudiante>();
+builder.Services.AddScoped<ServiceEstudiante>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,6 +38,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configurar middleware CORS
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -36,3 +56,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
